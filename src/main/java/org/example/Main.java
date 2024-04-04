78fc1fb7258037519576330215f7b49785a9cd0e
ArmandoGonzalez <ajgl0001@ce.pucmm.edu.do>
2024-04-03 20:13:40 -0400

    Importar javalin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "public";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress = false;
+                staticFileConfig.aliasCheck = null;
+            });
+        }).start(7000);
     }
 }
