74fbd1f3f854f05219abd9517e43b101575b71e8
ArmandoGonzalez <ajgl0001@ce.pucmm.edu.do>
2024-04-12 17:44:49 -0400

    Implementar servicio gRPC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import org.bson.types.ObjectId;
@@ -7,13 +9,27 @@ import org.example.clases.Usuario;
 import org.example.controllers.HomeController;
 import org.example.controllers.URLController;
 import org.example.controllers.UserController;
+import org.example.grpc.UrlServiceImpl;
 import org.example.services.UserServices;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         if (UserServices.getInstance().findByUsername("admin") == null) {
             UserServices.getInstance().crear(new Usuario(new ObjectId(),"admin", "admin", true));
         }
+        new Thread(() -> {
+            try {
+                Server server = ServerBuilder.forPort(50051)
+                        .addService(new UrlServiceImpl())
+                        .build();
+                server.start();
+                server.awaitTermination();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add(staticFileConfig -> {
                 staticFileConfig.hostedPath = "/";
